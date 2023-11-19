abb8c2d20e473abd9afe96f80278f04c2af9db68
Zach Willis <zachmw27@gmail.com>
2023-11-19 18:19:38 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superbowlWin(wins) {
+    const superbowlsWon = wins.find(record => record.result === "W");
+    return superbowlsWon ? superbowlsWon.year : undefined;
+}
+  
+const yearWon = superbowlWin(record);
+console.log(yearWon);
